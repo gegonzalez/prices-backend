5199825c7b61dd3ab8fb11d16c9009a2c0e819ca
German Gonzalez <gegonzalez@gmail.com>
2022-05-05 23:23:27 -0400

    feat(repository) adding price repository

---
+++ src/main/java/com/example/prices/repository/PriceRepository.java
@@ -0,0 +1,15 @@
+package com.example.prices.repository;
+
+import com.example.prices.domain.Price;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface PriceRepository extends CrudRepository<Price, Long> {
+    @Query("FROM Price p WHERE p.brandId = ?1 AND p.productId = ?2 AND p.startDate <= ?3 AND p.endDate >= ?3 ")
+    List<Price> findByProductIdAndBrandIdAndApplicationDate(int brandId, int productId, LocalDateTime applicationDate);
+}
