437520923db8a5925ec190dbf425c6509d754bf2
German Gonzalez <gegonzalez@gmail.com>
2022-05-15 22:02:09 -0400

    refactor(service) adding test error escenario

--- src/test/java/com/example/prices/service/PriceServiceTest.java
+++ src/test/java/com/example/prices/service/PriceServiceTest.java
@@ -5,14 +5,18 @@ import com.example.prices.entities.Price;
 import com.example.prices.repository.PriceRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.function.Executable;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.NoSuchElementException;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -32,5 +36,16 @@ class PriceServiceTest {
         Price priceList = priceService.fetchPrice(BRAND_ID, PRODUCT_ID, APPLICATION_DATE);
 
         assertSame(expectedPricesList.get(0), priceList);
+    }
+
+    @Test
+    void shouldThrowNoSuchElementException() {
+        when(priceRepository.findByProductIdAndBrandIdAndApplicationDate(BRAND_ID, PRODUCT_ID, APPLICATION_DATE))
+                .thenReturn(List.of());
+
+        NoSuchElementException thrown = assertThrows(NoSuchElementException.class,
+                                                    () -> priceService.fetchPrice(BRAND_ID, PRODUCT_ID, APPLICATION_DATE),
+                                            "NoSuchElementException was expected");
+        assertEquals("No value present", thrown.getMessage());
     }
 }
