edfce3c5a7f96c97d2965f12ebf7c00949ef7ad3
German Gonzalez <gegonzalez@gmail.com>
2022-05-22 19:21:31 -0400

    refactor(service) adding service interface

--- src/main/java/com/example/prices/service/PriceService.java
+++ src/main/java/com/example/prices/service/PriceService.java
@@ -1,30 +1,9 @@
 package com.example.prices.service;
 
 import com.example.prices.entities.Price;
-import com.example.prices.repository.PriceRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.Comparator;
 
-@Service
-public class PriceService {
-
-    @Autowired
-    private PriceRepository priceRepository;
-
-    private static final Logger LOG = LoggerFactory.getLogger(PriceService.class);
-
-    public Price fetchPrice(int brandId, int productId, LocalDateTime applicationDate) {
-        var message = String.format("action=fetchPrice, class=PriceService, brandId=%d, productId=%d, applicationDate=%s", brandId, productId, applicationDate.toString());
-        LOG.info(message);
-        var res = priceRepository.findByProductIdAndBrandIdAndApplicationDate(brandId, productId, applicationDate);
-        return res
-                .stream()
-                .max(Comparator.comparing(Price::getPriority))
-                .orElseThrow();
-    }
+public interface PriceService {
+    Price fetchPrice(int brandId, int productId, LocalDateTime applicationDate);
 }
